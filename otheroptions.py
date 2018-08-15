0d0b3cf75293d8b22b40f9357d55316854597d54
xjenny2 <40808846+xjenny2@users.noreply.github.com>
2018-08-15 13:37:24 -0400

    checked and coordinates adjusted for use with generalsvg_clinvar

--- otheroptions.py
+++ otheroptions.py
@@ -20,21 +20,20 @@ with open('YOUR-FILE') as tsv:
     ccrslist = fc.ccrs(ccrsfile)
 
 
-# at the end of the file, just before f.write('</svg>').  may have to change y-coordinates to adjust height
-f.write('<polygon points="0,170 ' + str(length + 2) + ',170 ' + str(length + 2) + ',200 0,200" />\n')
+# at the end of the file, just before f.write('</svg>').  for gnomad--may have to adjust y-coordinates
+f.write('<polygon points="0,270 ' + str(length + 2) + ',270 ' + str(length + 2) + ',300 0,300" />\n')
 
 for sublist in ccrslist:  # [position, #]
     ccrspos = str(sublist[0] * 3 - 1)  # scale
     percent = float(sublist[1]) / float(100)
     if sublist[1] == -1:  # plots white if no data
         f.write(
-            '\t<line x1="' + ccrspos + '" y1="172" x2="' + ccrspos + '" y2="198" '
-                                                                     'style="stroke: white; stroke-width: 4" />\n'
+            '\t<line x1="' + ccrspos + '" y1="272" x2="' + ccrspos + '" y2="298" '
+            'style="stroke: white; stroke-width: 4" />\n'
         )
     else:  # plots w/ color scale
         f.write(
-            '\t<line x1="' + ccrspos + '" y1="172" x2="' + ccrspos + '" y2="198" '
-                                                                     'style="stroke: rgb(' + str(
-                int((percent * 255))) + ',0,'
+            '\t<line x1="' + ccrspos + '" y1="272" x2="' + ccrspos + '" y2="298" '
+            'style="stroke: rgb(' + str(int((percent * 255))) + ',0,'
             + str(int((255 - (percent * 255)))) + '); stroke-width: 4" />\n'
         )
